611ba69a3330f1b037e16cc048a431c9104cc9a7
pedrohenao <105084079+pedrohenao@users.noreply.github.com>
2022-06-21 00:26:17 -0500

    Add files via upload

---
+++ Tarea3/car.js
@@ -0,0 +1,26 @@
+
+
+const products = [
+    {
+        name: 'Bicicleta montaña',
+        image:  './bici1.png',
+imagenes
+        price: 400,
+        quantity: 4,
+    },
+    {
+        name: 'Carro2',
+        image: './carro2.jpg',
+imagenes
+        price: 500,
+        quantity: 4,
+    },
+    {
+        name: 'Moto',
+        image: './moto.jpg', 
+imagenes
+        price: 200,
+        quantity: 6,
+    },
+    
+];
---
+++ Tarea3/index.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Compiled and minified CSS -->
+     <link
+     rel="stylesheet"
+     href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+   />
+
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          <img src="" alt="" />
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="./index.html">Home</a></li>
+          <li><a href="./card_products.html">Productos</a></li>
+          <li><a href="./new_product.html">Nuevo producto</a></li>
+
+        </ul>
+      </div>
+    </nav>
+    <div class="container"></div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html> 
